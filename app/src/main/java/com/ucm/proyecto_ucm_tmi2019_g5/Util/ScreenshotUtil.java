f2074ef9b4c42f6618b473e1b448b811168069ae
simonezacca <simone.zaccariello@gmail.com>
2019-04-24 20:27:50 +0200

    Implemented ScreenshotUtil and FileUtil in Scraping Activity (Simone | Santiago | Andrea )

---
+++ app/src/main/java/com/ucm/proyecto_ucm_tmi2019_g5/Util/ScreenshotUtil.java
@@ -0,0 +1,49 @@
+package com.ucm.proyecto_ucm_tmi2019_g5.Util;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewTreeObserver;
+
+public class ScreenshotUtil {
+    private static ScreenshotUtil mInstance;
+
+    private ScreenshotUtil() {
+    }
+
+    public static ScreenshotUtil getInstance() {
+        if (mInstance == null) {
+            synchronized (ScreenshotUtil.class) {
+                if (mInstance == null) {
+                    mInstance = new ScreenshotUtil();
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    /**
+     * Measures and takes a screenshot of the provided {@link View}.
+     *
+     * @param view The view of which the screenshot is taken
+     * @return A {@link Bitmap} for the taken screenshot.
+     */
+    public Bitmap takeScreenshotForView(View view) {
+        System.out.println("Valore view: " + view);
+        System.out.println("Valore getWidth: " + view.getWidth());
+        view.measure(MeasureSpec.makeMeasureSpec(view.getWidth(), MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(view.getHeight(), MeasureSpec.EXACTLY));
+        view.layout((int) view.getX(), (int) view.getY(), (int) view.getX() + view.getMeasuredWidth(), (int) view.getY() + view.getMeasuredHeight());
+
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache(true);
+        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+        view.setDrawingCacheEnabled(false);
+
+        return bitmap;
+    }
+
+    public Bitmap takeScreenshotForScreen(Activity activity) {
+        return takeScreenshotForView(activity.getWindow().getDecorView().getRootView());
+    }
+}
--- app/src/main/java/com/ucm/proyecto_ucm_tmi2019_g5/View/ScrapingActivity.java
+++ app/src/main/java/com/ucm/proyecto_ucm_tmi2019_g5/View/ScrapingActivity.java
@@ -1,27 +1,37 @@
 package com.ucm.proyecto_ucm_tmi2019_g5.View;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.ucm.proyecto_ucm_tmi2019_g5.R;
+import com.ucm.proyecto_ucm_tmi2019_g5.Util.FileUtil;
 import com.ucm.proyecto_ucm_tmi2019_g5.Util.Scraping;
+import com.ucm.proyecto_ucm_tmi2019_g5.Util.ScreenshotUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import static com.ucm.proyecto_ucm_tmi2019_g5.Util.Scraping.*;
+import static java.lang.Thread.sleep;
 
 
 public class ScrapingActivity extends AppCompatActivity {
 
     TextView tvMenu;
     private ImageView imageView;
-    protected View actualView;
+    private FileUtil fu;
+    private ImageView imageViewShowScreenshot;
+    private View actualView;
+
 
 
     @Override
@@ -29,8 +39,8 @@ public class ScrapingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scraping);
         tvMenu = findViewById(R.id.tv_menu);
+        imageViewShowScreenshot = findViewById(R.id.imageViewShowScreenshot);
         actualView = findViewById(android.R.id.content);
-
         /*
         Thread thread = new Thread(new Runnable() {
 
@@ -46,50 +56,32 @@ public class ScrapingActivity extends AppCompatActivity {
 
         thread.start();
         */
+
         Bundle extras = getIntent().getExtras();
         String menu = extras.getString("menu");
 //        System.out.println("menu in Scraping Activity: " + menu);
 
         tvMenu.setText(menu);
-        Bitmap b = takeScreenshot(actualView);
-//        imageView.setImageBitmap(b);
-    }
 
-    public Bitmap takeScreenshot(View v) {
-        System.out.println("sacamos screenshot");
-        v.setDrawingCacheEnabled(true);
-        v.buildDrawingCache(true);
-//        Bitmap b = Bitmap.createBitmap(v.getDrawingCache());
         try {
-
-            String mPath = Environment.getExternalStorageDirectory().toString() + "/foto.jpg";
-            System.out.println(mPath);
-            File imageFile = new File(mPath);
-            FileOutputStream outputStream = new FileOutputStream(imageFile);
-            Bitmap croping = Bitmap.createBitmap(v.getDrawingCache(), 0, 0, v.getDrawingCache().getWidth(), v.getDrawingCache().getHeight() - 105);
-            System.out.println("===============ESTAMOS AQUI===============");
-            croping.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-            outputStream.flush();
-            outputStream.close();
-            v.setDrawingCacheEnabled(false);
-            System.out.println("tenemos la foto");
-            return croping;
-        } catch (Throwable e) {
-            // Several error may come out with file handling or DOM
+            sleep(1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return null;
-    }
 
-//    private void takeScreenshot() {
-//
-//        try {
-//
-//            openScreenshot(imageFile);
-//        } catch (Throwable e) {
-//            // Several error may come out with file handling or DOM
-//            e.printStackTrace();
-//        }
-//    }
+        actualView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                actualView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                actualView.getHeight(); //height is ready
+                Bitmap bitmap = ScreenshotUtil.getInstance().takeScreenshotForView(actualView); // Take ScreenshotUtil for activity
+                imageViewShowScreenshot.setImageBitmap(bitmap);
+                String path = Environment.getExternalStorageDirectory().toString() + "/ScreenshotTest.png";
+                FileUtil.getInstance().storeBitmap(bitmap, path);
+            }
+        });
+
+//        imageView.setImageBitmap(b);
+    }
 
 }
